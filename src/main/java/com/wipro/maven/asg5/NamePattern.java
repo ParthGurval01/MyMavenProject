45b645b551b965ba66f962a2b3d228f76e595c44
ParthGurval01 <parthvinayak.gurval@wipro.com>
2025-12-10 17:56:12 +0530

    My Maven Project

---
+++ src/main/java/com/wipro/maven/asg5/NamePattern.java
@@ -0,0 +1,15 @@
+package com.wipro.maven.asg5;
+
+public class NamePattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "PARTH";
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println(name.substring(0, i + 1));
+        }
+
+	}
+
+}
